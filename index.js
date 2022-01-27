d63da23be410c0cf2b118a029c691f9070efb42d
nickiiitu <96411646+nickiiitu@users.noreply.github.com>
2022-01-27 19:51:27 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,31 +1,31 @@
 function sound(key){
     switch (key) {
         case "w":
-            var ad=new Audio("sounds/crash.mp3");
+            var ad=new Audio("crash.mp3");
             ad.play();
             break;
         case "a":
-            var ad=new Audio("sounds/kick-bass.mp3");
+            var ad=new Audio("kick-bass.mp3");
             ad.play();
             break;
         case "s":
-            var ad=new Audio("sounds/snare.mp3");
+            var ad=new Audio("snare.mp3");
             ad.play();
             break;
         case "d":
-            var ad=new Audio("sounds/tom-1.mp3");
+            var ad=new Audio("tom-1.mp3");
             ad.play();
             break;
         case "j":
-            var ad=new Audio("sounds/tom-2.mp3");
+            var ad=new Audio("tom-2.mp3");
             ad.play();
             break;
         case "k":
-            var ad=new Audio("sounds/tom-3.mp3");
+            var ad=new Audio("tom-3.mp3");
             ad.play();
             break;
         case "l":
-            var ad=new Audio("sounds/tom-4.mp3");
+            var ad=new Audio("tom-4.mp3");
             ad.play();
             break;
         default:
